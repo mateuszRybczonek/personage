1528709de2d198489783744c5c7fb80a332ad537
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-11-22 22:13:19 +0100

    Remove data-test- attributes on prod env

--- vue.config.js
+++ vue.config.js
@@ -49,5 +49,28 @@ module.exports = {
           ],
         },
       });
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => ({
+        ...options,
+        compilerOptions: {
+          ...options.compilerOptions,
+          modules: [{
+            preTransformNode(astEl) {
+              if (process.env.NODE_ENV === 'production' && !process.env.CYPRESS_ENV) {
+                const { attrsMap, attrsList } = astEl;
+                if (attrsMap['data-test']) {
+                  delete attrsMap['data-test'];
+                  const index = attrsList.findIndex(x => x.name === 'data-test');
+                  attrsList.splice(index, 1);
+                }
+              }
+              return astEl;
+            },
+          }],
+        },
+      }));
   },
 };
