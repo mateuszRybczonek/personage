c81d0243cc9c9cc2e0aa099b4e03ec725b37cd1b
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-11-22 22:12:44 +0100

    Add vue-svg-loader

--- vue.config.js
+++ vue.config.js
@@ -30,4 +30,24 @@ module.exports = {
       },
     },
   },
+
+  chainWebpack: (config) => {
+    config.plugins.delete('workbox');
+
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [
+            { removeViewBox: false },
+            { convertShapeToPath: false },
+          ],
+        },
+      });
+  },
 };
