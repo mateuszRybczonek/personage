906cbcf8759a8119957fbb2e0214bcefc6cf3cfd
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-11-26 11:30:01 +0100

    P-4 Copy emojis view from wordguru

---
+++ src/views/Emojis.vue
@@ -0,0 +1,33 @@
+<template>
+  <Wrapper :back="{name: 'setup'}">
+    <template slot="header">
+      {{ $t('views.emojis.title') }}
+    </template>
+
+    <EmojisContent />
+
+    <template slot="footer">
+      <button
+        class="btn btnPrimary"
+        @click="$_redirect({ name: 'categories' })"
+      >
+        {{ $t('views.emojis.choose_categories') }}
+      </button>
+      <button @click="$_redirect({ name: 'onboarding' })">
+        {{ $t('views.emojis.game_rules') }}
+      </button>
+    </template>
+  </Wrapper>
+</template>
+
+<script>
+import Wrapper from '@/components/Wrapper/Wrapper';
+import EmojisContent from '@/components/EmojisContent/EmojisContent';
+
+export default {
+  components: {
+    Wrapper,
+    EmojisContent,
+  },
+};
+</script>
