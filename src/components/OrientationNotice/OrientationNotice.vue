46027fd4048f7d87bce7a745e0c9c03418e9311a
Mateusz Rybczonek <mateusz.rybczonek@netguru.pl>
2018-12-03 18:45:29 +0100

    P-15 Add OrientationNotice component

---
+++ src/components/OrientationNotice/OrientationNotice.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="$style.orientationNotice">
+    <RotateIcon :class="$style.orientationNotice__image" />
+    <span>{{ 'Please turn your device to portait orientation' }}</span>
+  </div>
+</template>
+
+<script>
+import RotateIcon from '@/assets/rotate.svg';
+
+export default {
+  components: {
+    RotateIcon,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.orientationNotice {
+  display: flex;
+  height: 85vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  &__image {
+    @include sizing(150px);
+
+    fill: $c-white;
+  }
+}
+</style>
